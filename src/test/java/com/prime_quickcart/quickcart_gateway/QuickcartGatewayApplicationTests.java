521ae3af1728217877dc43ed5bc6c92bed31ece5
NalinPriyankara <nalinpriyankara033@gmail.com>
2025-05-28 13:03:22 +0530

    Initial commit

---
+++ src/test/java/com/prime_quickcart/quickcart_gateway/QuickcartGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prime_quickcart.quickcart_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcartGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
